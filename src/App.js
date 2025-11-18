1fa0f00b35618d494ea17a2a09b7c09dba86ae42
Kuldeep Joshi <kuldeep.joshi@sarvika.com>
2025-11-18 11:29:23 +0530

    React-Redux Basic workflow understanding Increment-Decrement Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.value);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="card">
+        <h1>Count: {count}</h1>
+
+        <button onClick={() => dispatch({ type: "INCREMENT" })}>
+          Increment
+        </button>
+
+        <button onClick={() => dispatch({ type: "DECREMENT" })}>
+          Decrement
+        </button>
+      </div>
     </div>
   );
 }
